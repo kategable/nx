1de5953f66387526bd63fe606d7afd48fef45353
Juri <juri.strumpflohner@gmail.com>
2025-02-01 08:34:07 +0100

    fix(nx-dev): update blog metadata to include the post descriptions

--- nx-dev/ui-blog/src/lib/blog-details.tsx
+++ nx-dev/ui-blog/src/lib/blog-details.tsx
@@ -11,26 +11,6 @@ export interface BlogDetailsProps {
   post: BlogPostDataEntry;
 }
 
-export async function generateMetadata({ post }: BlogDetailsProps) {
-  return {
-    title: post.title,
-    description: post.description,
-    openGraph: {
-      images: [
-        {
-          url: post.cover_image
-            ? `https://nx.dev${post.cover_image}`
-            : 'https://nx.dev/socials/nx-media.png',
-          width: 800,
-          height: 421,
-          alt: 'Nx: Smart Monorepos · Fast CI',
-          type: 'image/jpeg',
-        },
-      ],
-    },
-  };
-}
-
 export function BlogDetails({ post }: BlogDetailsProps) {
   const { node } = renderMarkdown(post.content, {
     filePath: post.filePath ?? '',
